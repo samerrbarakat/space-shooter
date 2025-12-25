956d945da3f2afc1cecf7e1365878daa22641cd7
samerrbarakat <barakatsamer932@gmail.com>
2025-12-25 20:15:38 +0200

    Setting up landing page

--- project/urls.py
+++ project/urls.py
@@ -21,5 +21,6 @@ from django.urls import include, path
 from django.views.generic import TemplateView
 urlpatterns = [
     path('admin/', admin.site.urls),
+    path("", TemplateView.as_view(template_name="landing.html"), name="landing"),
     path("game/", include("game.urls")),  # serves your game
 ]
